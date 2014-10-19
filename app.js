9baa7a074a05fc32a61e5bf92fcf586f49e8a466
Han Chang <szu.han.chang@gmail.com>
2014-10-19 09:07:11 -0700

    Use ng-repeat to add posts

--- app.js
+++ app.js
@@ -3,4 +3,10 @@ angular.module('reNews', [])
 '$scope',
 function ($scope) {
   $scope.test = 'Hello world!';
+  $scope.posts = [
+    'Housing starts on the rise',
+    'Interest rates at all time low',
+    'Population growth stabilizing',
+    'Proposition G in SF still being debated'
+  ];
 }]);
